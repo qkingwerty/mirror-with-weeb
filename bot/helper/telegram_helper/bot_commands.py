3c15d02e58b9b9b74f8d6af60c7f73349082cad1
Code With Weeb <107316620+codewithweeb@users.noreply.github.com>
2022-06-22 23:32:08 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -31,6 +31,7 @@ class _BotCommands:
         self.QbUnzipMirrorCommand = f'qbunzipmirror{CMD_INDEX}'
         self.QbZipMirrorCommand = f'qbzipmirror{CMD_INDEX}'
         self.DeleteCommand = f'del{CMD_INDEX}'
+        self.UsageCommand = f'usage{CMD_INDEX}'
         self.ShellCommand = f'shell{CMD_INDEX}'
         self.ExecHelpCommand = f'exechelp{CMD_INDEX}'
         self.LeechSetCommand = f'leechset{CMD_INDEX}'
