00e3fd8b18859e6be3429b3feb175dd3a397f187
Code With Weeb <107316620+codewithweeb@users.noreply.github.com>
2022-06-22 21:55:25 +0530

    Time stamp added

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -14,7 +14,7 @@ from telegram import InlineKeyboardMarkup
 from bot import Interval, INDEX_URL, BUTTON_FOUR_NAME, BUTTON_FOUR_URL, BUTTON_FIVE_NAME, BUTTON_FIVE_URL, \
                 BUTTON_SIX_NAME, BUTTON_SIX_URL, VIEW_LINK, aria2, QB_SEED, dispatcher, DOWNLOAD_DIR, \
                 download_dict, download_dict_lock, TG_SPLIT_SIZE, LOGGER, MEGA_KEY, DB_URI, INCOMPLETE_TASK_NOTIFIER
-from bot.helper.ext_utils.bot_utils import is_url, is_magnet, is_gdtot_link, is_mega_link, is_gdrive_link, get_content_type
+from bot.helper.ext_utils.bot_utils import is_url, is_magnet, is_gdtot_link, is_mega_link, is_gdrive_link, get_content_type, get_readable_time
 from bot.helper.ext_utils.fs_utils import get_base_name, get_path_size, split_file, clean_download
 from bot.helper.ext_utils.shortenurl import short_url
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException, NotSupportedExtractionArchive
@@ -150,7 +150,7 @@ class MirrorListener:
                         if not checked:
                             checked = True
                             with download_dict_lock:
-                                download_dict[self.uid] = SplitStatus(up_name, up_path, size, , self.message)
+                                download_dict[self.uid] = SplitStatus(up_name, up_path, size)
                             LOGGER.info(f"Splitting: {up_name}")
                         split_file(f_path, f_size, file_, dirpath, TG_SPLIT_SIZE)
                         osremove(f_path)
@@ -200,6 +200,7 @@ class MirrorListener:
             msg += f'\n<b>├📚 Total Files: </b>{folders}'
             if typ != 0:
                 msg += f'\n<b>├💀 Corrupted Files: </b>{typ}'
+            msg += f'\n<b>├⌛ It Tooks:</b> {get_readable_time(time() - self.message.date.timestamp())}'
             msg += f'\n<b>╰👤 cc: </b>{self.tag}\n'
             if not files:
                 sendMessage(msg, self.bot, self.message)
@@ -218,6 +219,7 @@ class MirrorListener:
             if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
                 msg += f'\n<b>├🗃️ SubFolders: </b>{folders}'
                 msg += f'\n<b>├🗂️ Files: </b>{files}'
+            msg += f'\n<b>├⌛ It Tooks:</b> {get_readable_time(time() - self.message.date.timestamp())}'
             msg += f'\n<b>╰👤 cc: </b>{self.tag}'
             buttons = ButtonMaker()
             link = short_url(link)
